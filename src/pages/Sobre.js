7afc65e571ced05ac161f029b7bda95bb2f88872
Philipe Lima <philipelima803@gmail.com>
2021-11-13 21:14:19 -0300

    att do modal

--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -3,11 +3,19 @@ import React from "react";
 function Sobre(){
  return(
      <div> 
+         
          <h1> Bruno Amorim</h1>
          <h1> Davi Gonçalves  </h1>
          <h1> Henthony Guilherme </h1>
          <h1> Luan Felipe</h1>
+         
+         <div>
          <h1> Philipe Lima </h1>
+
+         <a href="https://github.com/PhilipeLima123?tab=repositories" target="_blank" rel="noopener noreferrer"></a>
+         <a href="https://www.linkedin.com/in/philipe-lima-470712214/" target="_blank" rel="noopener noreferrer"></a>
+         </div>
+
 
      </div>
  )
