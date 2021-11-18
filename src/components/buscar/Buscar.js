d7b875f695e85d21c7542d29e41f592d81091583
Luan Felipe <luan0412@outlook.com>
2021-11-18 00:32:16 -0300

    Filtragem de amigos

--- src/components/buscar/Buscar.js
+++ src/components/buscar/Buscar.js
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './Buscar.css'
 import * as BiIcons from 'react-icons/bi';
 
 
 function BarraDeBuscar() {
+
+    
+
+    const filtrar = (e) => {
+        if(e.target.value && e.target.value.length >= 3){
+            let historico = JSON.parse(localStorage.getItem("amigos_free"));
+            if (historico) {
+                let results =  historico.filter((item)=>{
+                    return item.name.first.includes(e.target.value) == true;
+                });
+                localStorage.setItem("amigos_free_filter", JSON.stringify(results)) 
+            }else{
+                localStorage.setItem("amigos_free_filter", {})
+            }
+        }else{
+            localStorage.setItem("amigos_free_filter", {})
+
+        }
+
+}
     return (
         <div className="boxBusca">
             <label for="buscar">
                 <i className="icon"><BiIcons.BiSearchAlt className="ico" /></i>
             </label>
 
-            <input type="text" placeholder="Buscar..." id="buscar" className="buscar" />
+            <input type="text" autoComplete onChange={filtrar} placeholder="Filtrar por nome.." id="buscar" className="buscar" />
         </div>
     );
 }
+
 
 export default BarraDeBuscar;
--- src/pages/Amigos.js
+++ src/pages/Amigos.js
@@ -2,32 +2,57 @@ import React, { useState, useEffect } from 'react';
 import '../css/Amigos.css';
 import LocalModal from '../components/modal/localModal.js';
 import RandomUserApi from "../services/RandomUserApi";
-import '../components/modal/styles.scss'
-import iconeAmigos from "../source/iconAmigos.png"
+import '../components/modal/styles.scss';
+import '../components/buscar/Buscar.css';
+import * as BiIcons from 'react-icons/bi';
+
 import Navbar from '../components/navbar/Navbar';
 
 function Amigos() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [user, setUser] = useState(null)
   const [dados, setDados] = useState([]);
- 
+  const [dadosIniciais, setDadosIniciais] = useState([]);
+  const [isMensagemVisible, setIsMensagemVisible] = useState(false);  
 
   useEffect(() => {
     // Atualiza o titulo do documento usando a API do browser 
+    getApi();
+  }, []);
+  
+  const getApi = () =>{
     let url = "?results=54&seed=666";
 
     RandomUserApi.get(url)
       .then((response) => {
         if (response.status >= 200 && response.status < 300) {
+          setDadosIniciais(response.data.results)
           setDados(response.data.results);
         } else {
+          setDadosIniciais([]);
           setDados([]);
         }
       })
       .catch((error) => {
+        setDadosIniciais([]);
         setDados([]);
       });
-  }, []);
+  }
+
+  const filtrar = (e) => {
+      if(e.target.value && e.target.value.length >= 3){
+         if (dadosIniciais && dadosIniciais.length > 0) {
+              let results =  dadosIniciais.filter((item)=>{
+                  return item.name.first.toLowerCase().includes(e.target.value.toLowerCase());
+              });
+             setDados(results);
+          } else{
+            setDados(dadosIniciais);
+          }
+      } else{
+          setDados(dadosIniciais);
+      }
+  }
 
   const setUsuarioEspecifico = (user) => {
     setIsModalVisible(true)
@@ -36,58 +61,71 @@ function Amigos() {
 
   return (
 
-    <>  
-    <Navbar/>
-    <h1 id="titulo">Amigos </h1> 
-    
+    <>
+      <Navbar />
+      <div className="boxBusca">
+          <label for="buscar">
+              <i className="icon"><BiIcons.BiSearchAlt className="ico" /></i>
+          </label>
 
-    <div id="master">
-    <title>Amigos</title>
-     
+          <input type="text" onChange={filtrar} placeholder="Filtrar por nome.." id="buscar" className="buscar" />
+      </div>
+      <h1 id="titulo">Amigos </h1>
 
-      
-      <div className="mainDiv">
+      <div id="master">
+        <title>Amigos</title>
 
-      {dados.map(
-        (item, index) => {
-          return <div key={index}>
+        <div className="mainDiv">
 
-              <div className="mestreDiv">
-                <div className="textosDiv"> 
-            <div className="nomeDiv"> <p id="nome" className="skeleton skeleton-text"> <span id="spanEdit"> Nome: </span> {item.name.first}</p></div>
-            <div className="usuariosDiv"> <p id="usuarios" className="skeleton skeleton-text"> <span id="spanEdit"> Usuário: </span> {item.login.username}</p></div>
-            </div>
-            
-            <div className="imagensDiv"> <img id="imagens" className="skeleton" onClick={() => setUsuarioEspecifico(item)} src={item.picture.large} alt={item.name.first} /> </div>
-            </div>
+          {dados.map(
+            (item, index) => {
+              return <div key={index}>
 
+                <div className="mestreDiv">
+                  <div className="textosDiv">
+                    <div className="nomeDiv"> <p id="nome" className="skeleton skeleton-text"> <span id="spanEdit"> Nome: </span> {item.name.first}</p></div>
+                    <div className="usuariosDiv"> <p id="usuarios" className="skeleton skeleton-text"> <span id="spanEdit"> Usuário: </span> {item.login.username}</p></div>
+                  </div>
 
-            {isModalVisible ? (
-
-              <LocalModal onClose={() => setIsModalVisible(false)}>
-                 <div className="imagemModalDiv"><img id="imagemModal" src={user.picture.large} alts={user.name.first} /></div> 
-                  <div className="modalClassAll">
-                <div className="modalClass">
-               
-               <div className="textos"><p id="nome"> <span id="spanEdit"> Nome: </span> {user.name.first}</p> 
-               <p id="sobrenome"> <span id="spanEdit"> Sobrenome: </span> {user.name.last}</p> 
-               <p id="pais"> <span id="spanEdit"> País:</span> {user.location.country}</p> 
-               <p id="usuario"> <span id="spanEdit"> Usuário: </span> {user.login.username}  </p> 
-               <div id="divBotaoM"><button id="mensagemBotao"> Enviar mensagem </button></div>
-               </div> 
-               </div>
+                  <div className="imagensDiv"> <img id="imagens" className="skeleton" onClick={() => setUsuarioEspecifico(item)} src={item.picture.large} alt={item.name.first} /> </div>
                 </div>
-                
-              </LocalModal>) : null}
-              
-          </div>
 
-        }
-      )
-      }
+
+                {isModalVisible ? (
+
+                  <LocalModal onClose={() => setIsModalVisible(false)}>
+                    <div className="imagemModalDiv"><img id="imagemModal" src={user.picture.large} alts={user.name.first} /></div>
+                    <div className="modalClassAll">
+                      <div className="modalClass">
+
+                        <div className="textos"><p id="nome"> <span id="spanEdit"> Nome: </span> {user.name.first}</p>
+                          <p id="sobrenome"> <span id="spanEdit"> Sobrenome: </span> {user.name.last}</p>
+                          <p id="pais"> <span id="spanEdit"> País:</span> {user.location.country}</p>
+                          <p id="usuario"> <span id="spanEdit"> Usuário: </span> {user.login.username}  </p>
+                          <div id="divBotaoM"> 
+                              <button id="mensagemBotao" onClick={() => setIsMensagemVisible(true)}> Escrever mensagem </button>
+                              {  isMensagemVisible === true ? 
+                                  <div>
+                                      <textarea></textarea>
+                                      <button>enviar</button>  <button onClick={() => setIsMensagemVisible(false)}>cancelar</button>
+                                  </div>
+                                  : ""
+                              }
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                  </LocalModal>) : null}
+
               </div>
 
-    </div>
+            }
+          )
+          }
+        </div>
+
+      </div>
     </>
 
   );
