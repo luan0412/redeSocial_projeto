b09ced96dd63c50362be021856967c4590f035ed
Luan Felipe <luan0412@outlook.com>
2021-11-17 22:47:30 -0300

    Testando a fucionalidade Filtrar

--- src/pages/Amigos.js
+++ src/pages/Amigos.js
@@ -3,14 +3,14 @@ import '../css/Amigos.css';
 import LocalModal from '../components/modal/localModal.js';
 import RandomUserApi from "../services/RandomUserApi";
 import '../components/modal/styles.scss'
-import iconeAmigos from "../source/iconAmigos.png"
+
 import Navbar from '../components/navbar/Navbar';
 
 function Amigos() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [user, setUser] = useState(null)
   const [dados, setDados] = useState([]);
- 
+
 
   useEffect(() => {
     // Atualiza o titulo do documento usando a API do browser 
@@ -19,75 +19,99 @@ function Amigos() {
     RandomUserApi.get(url)
       .then((response) => {
         if (response.status >= 200 && response.status < 300) {
-          setDados(response.data.results);
+          setHistorico(response.data.results)
+
         } else {
-          setDados([]);
+          setHistorico([]);
         }
       })
       .catch((error) => {
         setDados([]);
       });
   }, []);
+  
+  
+  // useEffect(() => {
+  //   let historico = JSON.parse(localStorage.getItem("amigos_free_filter"));
+  //       if (historico) {
+  //         setHistorico(historico)
+  //     }else{
+  //         setHistorico(dados)
+  //     }
+  // });
+
+
+  const setHistorico = (results) =>{
+    let historico = JSON.parse(localStorage.getItem("amigos_free"));
+    if (historico) {
+      setDados(historico); 
+    } else {
+      setDados(results);
+      localStorage.setItem("amigos_free", JSON.stringify(results))
+    }
+  }
+
 
   const setUsuarioEspecifico = (user) => {
     setIsModalVisible(true)
     setUser(user)
+
   }
 
   return (
 
-    <>  
-    <Navbar/>
-    <h1 id="titulo">Amigos </h1> 
-    
-
-    <div id="master">
-    <title>Amigos</title>
-     
-
-      
-      <div className="mainDiv">
-
-      {dados.map(
-        (item, index) => {
-          return <div key={index}>
-
-              <div className="mestreDiv">
-                <div className="textosDiv"> 
-            <div className="nomeDiv"> <p id="nome" className="skeleton skeleton-text"> <span id="spanEdit"> Nome: </span> {item.name.first}</p></div>
-            <div className="usuariosDiv"> <p id="usuarios" className="skeleton skeleton-text"> <span id="spanEdit"> Usuário: </span> {item.login.username}</p></div>
-            </div>
-            
-            <div className="imagensDiv"> <img id="imagens" className="skeleton" onClick={() => setUsuarioEspecifico(item)} src={item.picture.large} alt={item.name.first} /> </div>
-            </div>
+    <>
+      <Navbar />
+      <h1 id="titulo">Amigos </h1>
 
 
-            {isModalVisible ? (
+      <div id="master">
+        <title>Amigos</title>
 
-              <LocalModal onClose={() => setIsModalVisible(false)}>
-                 <div className="imagemModalDiv"><img id="imagemModal" src={user.picture.large} alts={user.name.first} /></div> 
-                  <div className="modalClassAll">
-                <div className="modalClass">
-               
-               <div className="textos"><p id="nome"> <span id="spanEdit"> Nome: </span> {user.name.first}</p> 
-               <p id="sobrenome"> <span id="spanEdit"> Sobrenome: </span> {user.name.last}</p> 
-               <p id="pais"> <span id="spanEdit"> País:</span> {user.location.country}</p> 
-               <p id="usuario"> <span id="spanEdit"> Usuário: </span> {user.login.username}  </p> 
-               <div id="divBotaoM"><button id="mensagemBotao"> Enviar mensagem </button></div>
-               </div> 
-               </div>
+
+
+        <div className="mainDiv">
+
+          {dados.map(
+            (item, index) => {
+              return <div key={index}>
+
+                <div className="mestreDiv">
+                  <div className="textosDiv">
+                    <div className="nomeDiv"> <p id="nome" className="skeleton skeleton-text"> <span id="spanEdit"> Nome: </span> {item.name.first}</p></div>
+                    <div className="usuariosDiv"> <p id="usuarios" className="skeleton skeleton-text"> <span id="spanEdit"> Usuário: </span> {item.login.username}</p></div>
+                  </div>
+
+                  <div className="imagensDiv"> <img id="imagens" className="skeleton" onClick={() => setUsuarioEspecifico(item)} src={item.picture.large} alt={item.name.first} /> </div>
                 </div>
-                
-              </LocalModal>) : null}
-              
-          </div>
 
-        }
-      )
-      }
+
+                {isModalVisible ? (
+
+                  <LocalModal onClose={() => setIsModalVisible(false)}>
+                    <div className="imagemModalDiv"><img id="imagemModal" src={user.picture.large} alts={user.name.first} /></div>
+                    <div className="modalClassAll">
+                      <div className="modalClass">
+
+                        <div className="textos"><p id="nome"> <span id="spanEdit"> Nome: </span> {user.name.first}</p>
+                          <p id="sobrenome"> <span id="spanEdit"> Sobrenome: </span> {user.name.last}</p>
+                          <p id="pais"> <span id="spanEdit"> País:</span> {user.location.country}</p>
+                          <p id="usuario"> <span id="spanEdit"> Usuário: </span> {user.login.username}  </p>
+                          <div id="divBotaoM"> <button id="mensagemBotao"> Escrever mensagem </button></div>
+                        </div>
+                      </div>
+                    </div>
+
+                  </LocalModal>) : null}
+
               </div>
 
-    </div>
+            }
+          )
+          }
+        </div>
+
+      </div>
     </>
 
   );
